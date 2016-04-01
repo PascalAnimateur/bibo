0647d7b3a3d7b5ab339e38cbe76fc66e2354aba5
Pascal Martineau <pascal.animateur@gmail.com>
2016-04-01 16:15:12 -0400

    Initial default (table) and APA reference view modes for work entities

---
+++ theme/bibo-work--book--apa.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Default APA theme implementation for work entity.
+ *
+ * Available variables:
+ * - $title: The (sanitized) entity label.
+ */
+?>
+
+<?php print $title; ?>
