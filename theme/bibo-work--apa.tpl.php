4227e9fc2272ae7abd26deaee8f3c3b200f06b87
Pascal Martineau <pascal.animateur@gmail.com>
2016-04-04 12:17:42 -0400

    Initial RDF mappings for BIBO fields

--- theme/bibo-work--apa.tpl.php
+++ theme/bibo-work--apa.tpl.php
@@ -8,5 +8,14 @@
  */
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php print $author; ?> (<?php print $publication_status; ?>). <strong><?php print $title; ?>.</strong>
+  (<?php print render($content['bibo_publication_status']); ?>).
+  <span <?php print $title_attributes; ?>>
+    <?php if ($url): ?>
+      <a href="<?php print $url; ?>"><?php print $title; ?></a>
+    <?php else: ?>
+      <?php print $title; ?>
+    <?php endif; ?>
+  </span>
+  <?php print render($content['bibo_publisher']); ?>: 
+  <?php print render($content['bibo_place']); ?>
 </div>
