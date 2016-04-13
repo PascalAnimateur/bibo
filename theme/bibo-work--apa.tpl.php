bf9cfc37e200973f6f638a16b1a5fcc121dfc059
Pascal Martineau <pascal.animateur@gmail.com>
2016-04-13 13:39:23 -0400

    Trying new approach in APA work template

--- theme/bibo-work--apa.tpl.php
+++ theme/bibo-work--apa.tpl.php
@@ -8,7 +8,18 @@
  */
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php
+  // Authors.
+  if (isset($content['author'])) {
+    foreach ($content['author'] as $author) {
+      print render($author);
+    }
+  }
+  ?>
+
   (<?php print render($content['bibo_publication_status']); ?>).
+
   <span <?php print $title_attributes; ?>>
     <?php if ($url): ?>
       <a href="<?php print $url; ?>"><?php print $title; ?></a>
@@ -16,6 +27,16 @@
       <?php print $title; ?>
     <?php endif; ?>
   </span>
-  <?php print render($content['bibo_publisher']); ?>: 
-  <?php print render($content['bibo_place']); ?>
+
+  <?php
+  // Place: Publisher.
+  if (isset($content['bibo_place'])) {
+    print render($content['bibo_place']);
+    print isset($content['bibo_publisher']) ? ': ' : '';
+  }
+  if (isset($content['bibo_publisher'])) {
+    print render($content['bibo_publisher']) . '.';
+  }
+  ?>
+
 </div>
